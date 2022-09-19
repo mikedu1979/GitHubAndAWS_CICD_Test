ff4ef32ed054b2a919fbf457a09f8fb4b79b1d45
zzhhsshh <zzhhsshh@126.com>
2022-09-19 16:28:36 +0800

    CICD Demo

---
+++ app.js
@@ -0,0 +1,55 @@
+var express = require('express');
+
+var app = express();
+var bodyParser = require('body-parser');
+
+//引用bodyParser
+app.use(bodyParser.json()); // for parsing application/json
+
+app.use(bodyParser.urlencoded({
+    extended: true
+})); // for parsing application/x-www-form-urlencoded
+
+//设置跨域访问
+app.all('*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By", ' 3.2.1');
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
+
+var questions = [
+    {
+        data: 2022091901,
+        num: 123456789,
+        age: 30
+    },
+    {
+        data: 2022091902,
+        num: 987654321,
+        age: 40
+    }
+];
+
+//写个接口123
+app.get('/getino', function (req, res) {
+    res.status(200),
+        res.json(questions)
+});
+
+app.post('/postTest', function (req, res) {
+    console.log(req.stack);
+    console.log(req.body);
+    console.log(req.url);
+    console.log(req.query);
+    res.json(req.body)
+})
+
+//配置服务端口
+var server = app.listen(3001, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Example app listening at http://%s:%s', host, port);
+})
